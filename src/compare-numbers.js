2a825af4a4b7c85f78962e7b4075d4e7a340e8f2
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-17 11:24:58 -0700

    improving JavaScript function

--- src/compare-numbers.js
+++ src/compare-numbers.js
@@ -1,13 +1,19 @@
 function compareNumbers(guess, correctNumber) {
+    let hint = '';
     if(guess === correctNumber) {
+        hint = 'Your number is correct';
         return 0;
     }
     else if(guess < correctNumber) {
+        hint = 'Your number is too low';
         return -1;
+        
     }
     else if(guess > correctNumber) {
+        hint = 'Your number is too high';
         return 1;
     }
+    return hint;
 }
 
 export default compareNumbers;
--- src/guess.js
+++ src/guess.js
@@ -5,11 +5,11 @@ const guess = document.getElementById('guess');
 const guessButton = document.getElementById('guess-button');
 const remainingTries = document.getElementById('remaining-tries');
 const tooHighLow = document.getElementById('too-high-low');
+const winLose = document.getElementById('win-or-lose');
 
 // defining variables for state
 const correctNumber = 17;
 let remaining = 4;
-let hint = '';
 
 // event handlers
 guessButton.addEventListener('click', () => {
@@ -17,13 +17,22 @@ guessButton.addEventListener('click', () => {
     remaining -= 1;
 
     remainingTries.textContent = remaining;
-
+    
     if(remaining === 0) {
         guessButton.disabled = true;
+        winLose.textContent = 'Loser';
     }
 
-    if(guess > correctNumber) {
-        
+    if(guess.value > correctNumber) {
+        tooHighLow.textContent = compareNumbers();
     }
+    else if(guess.value < correctNumber) {
+        tooHighLow.textContent = compareNumbers();
+    }
+    else if(parseInt(guess.value) === correctNumber) {
+        guessButton.disabled = true;
+        winLose.textContent = 'Winner';
+    }
+    
 
-})
+});
